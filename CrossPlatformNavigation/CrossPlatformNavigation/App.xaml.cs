05dc42f909f9748a1378f7de61bbb3e8042a0833
Erik Král <ekral@fai.utb.cz>
2017-12-03 17:43:12 +0100

    Init release

---
+++ CrossPlatformNavigation/CrossPlatformNavigation/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace CrossPlatformNavigation
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainView view = new MainView();
+            MainViewModel viewModel = new MainViewModel(view.Navigation);
+            view.BindingContext = viewModel;
+
+			MainPage = new NavigationPage (view);
+
+            // Smazat pokud chci vychozi kulturu na telefonu
+            CultureInfo czechCulture = new CultureInfo("cs-CZ");
+            CultureInfo.DefaultThreadCurrentCulture = czechCulture;
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ CrossPlatformNavigation/CrossPlatformNavigation/ViewModel/KalendarViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformNavigation
+{
+    class KalendarViewModel : ViewModelBase
+    {
+        public string Test { get; set; } = "ahoj";
+
+        private List<KalendarItem> _kalendar;
+        
+        public List<KalendarItem> Kalendar
+        {
+            get { return _kalendar; }
+            set { _kalendar = value; OnPropertyChange(); }
+        }
+
+        public RelayCommand CommandZpet { get; set; }
+
+        private readonly Xamarin.Forms.INavigation navigation;
+
+        public KalendarViewModel(Xamarin.Forms.INavigation navigation, double dluh, double rocniUrok, int pocetLet)
+        {
+            this.navigation = navigation;
+            CommandZpet = new RelayCommand(async parameter => await NavigateZpet(parameter), null);
+
+            List<KalendarItem> kalendar = new List<KalendarItem>();
+
+            int pocetMesicu = pocetLet * 12; // pocet mesicu splaceni
+            double mesicniSazba = rocniUrok / (12 * 100); // desetinne cislo
+            double v = 1 / (1 + mesicniSazba);
+            double splatka = (mesicniSazba * dluh) / (1 - Math.Pow(v, pocetMesicu));
+
+            for (int i = 0; i < pocetMesicu; i++)
+            {
+                double urok = mesicniSazba * dluh;
+                double umor = splatka - urok;
+
+                dluh -= umor;
+
+                kalendar.Add(new KalendarItem(i, splatka, urok, umor, dluh));
+            }
+
+            Kalendar = kalendar;
+            
+        }
+
+        async Task NavigateZpet(object parameter)
+        {
+            await navigation.PopAsync();
+        }
+    }
+}
